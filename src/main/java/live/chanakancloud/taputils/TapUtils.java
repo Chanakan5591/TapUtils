363032f3a95afcb0952a6e0e0353ab98b7cf36cf
Chanakan Mungtin <chanakan5591@chanakancloud.live>
2021-04-29 12:50:02 +0700

    Removed Unused Getter-Setter

--- src/main/java/live/chanakancloud/taputils/TapUtils.java
+++ src/main/java/live/chanakancloud/taputils/TapUtils.java
@@ -10,6 +10,5 @@ public class TapUtils {
     @Getter @Setter private static JavaPlugin plugin;
     @Getter @Setter private static ChatColor prefixColor;
     @Getter @Setter private static String pluginName;
-    @Getter @Setter private static String pastebin_api_user_key;
 
 }
