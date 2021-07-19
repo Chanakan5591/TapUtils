324acfe8d63bf23b33dc06a8ca53abd634ad24a7
Chanakan Mungtin <chanakan5591@chanakancloud.live>
2021-07-19 08:17:34 +0700

    added withAir boolean parameter on block scan function

--- src/main/java/live/chanakancloud/taputils/utils/BlockUtils.java
+++ src/main/java/live/chanakancloud/taputils/utils/BlockUtils.java
@@ -1,6 +1,7 @@
 package live.chanakancloud.taputils.utils;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
@@ -8,27 +9,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BlockUtils {
-    /** to get friction of all ice blocks
+    /**
+     * to get friction of all ice blocks
      *
      * @param material the Material to be check
      * @return float
      */
     public static float getFriction(XMaterial material) {
-        switch(material) {
-            case SLIME_BLOCK:
-                return 0.8f;
-            case ICE:
-            case PACKED_ICE:
-            case FROSTED_ICE:
-                return 0.98f;
-            case BLUE_ICE:
-                return 0.989f;
-            default:
-                return 0.6f;
-        }
+        return switch (material) {
+            case SLIME_BLOCK -> 0.8f;
+            case ICE, PACKED_ICE, FROSTED_ICE -> 0.98f;
+            case BLUE_ICE -> 0.989f;
+            default -> 0.6f;
+        };
     }
 
-    /** get the blocks at the top of player
+    /**
+     * get the blocks at the top of player
      *
      * @param player the player to be check
      * @return Block, null if not exist after 256 blocks checking
@@ -36,15 +33,16 @@ public class BlockUtils {
     public static Block getTopBlock(Player player) {
         Location loc = player.getLocation().clone();
         double y = loc.getBlockY();
-        for (double i = y; i >= 0; i++){
+        for (double i = y; i >= 0; i++) {
             loc.setY(i);
-            if(loc.getBlock().getType().isSolid()) return loc.getBlock();
-            if(loc.getY() >= 256) break;
+            if (loc.getBlock().getType().isSolid()) return loc.getBlock();
+            if (loc.getY() >= 256) break;
         }
         return null;
     }
 
-    /** get the blocks at the top of another block
+    /**
+     * get the blocks at the top of another block
      *
      * @param block the block to be check
      * @return Block, null if not exist after 256 blocks checking
@@ -55,34 +53,40 @@ public class BlockUtils {
         for (double i = y; i >= 0; i++) {
             loc.setY(i);
             if (loc.getBlock().getType().isSolid()) return loc.getBlock();
-            if(loc.getY() >= 256) break;
+            if (loc.getY() >= 256) break;
         }
         return null;
     }
 
-    /** get block around player within radius
+    /**
+     * get block around player within radius
      *
      * @param player the player to be check
      * @param radius radius of blocks be scan.
      * @return List<Block>
      */
-    public static List<Block> getBlockInRadius(Player player, int radius) {
+    public static List<Block> getBlockInRadius(Player player, int radius, boolean withAir) {
         List<Block> blocks = new ArrayList<>();
         Block block = player.getLocation().getBlock();
 
-        for (int x = -(radius); x <= radius; x ++) {
+        for (int x = -(radius); x <= radius; x++) {
             for (int y = -(radius); y <= radius; y++) {
                 for (int z = -(radius); z <= radius; z++) {
-                    blocks.add(block.getRelative(x, y, z));
+                    if (!withAir && block.getRelative(x, y, z).getType() != Material.AIR) {
+                        blocks.add(block.getRelative(x, y, z));
+                    } else if(withAir) {
+                        blocks.add(block.getRelative(x, y, z));
+                    }
                 }
             }
         }
         return blocks;
     }
 
-    /** get block around block within radius
+    /**
+     * get block around block within radius
      *
-     * @param block the block to be check
+     * @param block  the block to be check
      * @param radius radius of blocks be scan.
      * @return List<Block>
      */
@@ -92,14 +96,15 @@ public class BlockUtils {
         for (int x = -(radius); x <= radius; x++) {
             for (int y = -(radius); y <= radius; y++) {
                 for (int z = -(radius); z <= radius; z++) {
-                    blocks.add(block.getRelative(x,y,z));
+                    blocks.add(block.getRelative(x, y, z));
                 }
             }
         }
         return blocks;
     }
 
-    /** get the block at the ground of player
+    /**
+     * get the block at the ground of player
      *
      * @param player the player to be check
      * @return Block, null if none
@@ -107,16 +112,17 @@ public class BlockUtils {
     public static Block getBlockFromGround(Player player) {
         Location loc = player.getLocation().clone();
         double y = loc.getBlockY();
-        for (double i = y; i >= 0; i--){
+        for (double i = y; i >= 0; i--) {
             loc.setY(i);
-            if(loc.getBlock().getType().isSolid()) {
+            if (loc.getBlock().getType().isSolid()) {
                 return loc.getBlock();
             }
         }
         return null;
     }
 
-    /** get the block at the ground of another block
+    /**
+     * get the block at the ground of another block
      *
      * @param block the block to be check
      * @return Block, null if none
